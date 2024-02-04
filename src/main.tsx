96db25d7a1ef10b1c683625e67a6c0fd99394a41
Ivan Khilevich <vanyahilevich@gmail.com>
2024-02-04 18:38:07 +0300

    setting project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from './App';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <Router>
+        <App/>
+    </Router>
+);
