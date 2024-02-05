01c5ccba5072250fd4a1e9792afab349b95cd99a
Pavel <pavel.1999.makhnach@gmail.com>
2024-02-05 22:00:51 +0300

    config setting and link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-import {Link} from "react-router-dom";
+
 import './index.css';
+import Link from "./components/Link";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='font-mainFont'>
+        <Link>main</Link>
+        <Link>products</Link>
+        <Link>contact</Link>
+        <Link>about</Link>
+
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
         </a>
@@ -18,7 +25,7 @@ function App() {
           <div className="text-red-500 p-4 m-6">
               Это компонент с использованием Tailwind CSS!
           </div>
-          <Link to={"/product"}>menuuuuuu</Link>
+
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -32,7 +39,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
